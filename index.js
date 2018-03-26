ce40685d972a8ceb5f537013a56f5bff56b64018
hongyq <415453941@qq.com>
2018-03-26 13:40:41 +0800

    init

---
+++ index.js
@@ -0,0 +1,8 @@
+export function disAction(model) {
+  let actions = {
+    [model.name]: (state, { payload }) => ({ ...state, ...payload })
+  };
+  return (state = model.state, action) => {
+    return Object.keys(actions).indexOf(action.type) > -1 ? actions[action.type](state, action) : state;
+  };
+}
